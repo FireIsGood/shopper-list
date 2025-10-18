9362243820dc88c314c87d831cdea84d7528b048
FireIsGood <fireisadequate@gmail.com>
2025-10-17 17:54:22 -0700

    feat: about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+  <title>Shopper List &mdash; About</title>
+</svelte:head>
+
+<article>
+  <section>
+    <h2>What is Shopper List?</h2>
+    <p>Shopper List is an app to track groceries you want to buy at various stores across your devices.</p>
+    <h2>How are lists stored?</h2>
+    <p>
+      Currently lists are stored on device in your browser's Local Storage, however there are plans to allow for
+      cross-device data storage via accounts. This is not implemented yet.
+    </p>
+  </section>
+</article>
