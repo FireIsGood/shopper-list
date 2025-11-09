29b627d7d15cdfe76537419066256324fd5021ef
FireIsGood <fireisadequate@gmail.com>
2025-11-09 13:58:57 -0800

    feat: more about page answers

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,17 +4,38 @@
 
   type Tutorial = {
     title: string;
-    description: string;
+    description: string[];
   };
 
   const tutorials: Tutorial[] = [
     {
       title: "Add an item",
-      description: "hi",
+      description: [
+        `To add an item, click the 'Add Item' button in the bottom of the Dashboard or individual items pages.`,
+      ],
     },
     {
-      title: "Mark an item as bought",
-      description: "hi",
+      title: "Mark an item as Bought",
+      description: [
+        `To mark an item as bought from the app Dashboard page's To Buy section, click the checkmark icon to the right
+of the item.`,
+        `To mark an item as bought from the To Buy or All Items pages' cards, click the 'Mark Bought' button.`,
+      ],
+    },
+    {
+      title: "Mark an item as To Buy",
+      description: [
+        `To mark an item as To Buy, navigate to the All Items page and click the item's 'Mark to Buy' button`,
+      ],
+    },
+    {
+      title: "Edit an item's name, description, or stores",
+      description: [
+        `To open the Edit Item dialog from the app's Dashboard page's To Buy section, click on the pencil icon.`,
+        `To open the Edit Item dialog from the app's To Buy or All Items pages, click on the 'Edit Item' button.`,
+        `Inside the Edit Item dialog you can write different values in the item's name, description or stores fields and
+then click the 'Save' button.`,
+      ],
     },
   ];
 </script>
@@ -28,7 +49,7 @@
     <h2>Frequently Asked Questions</h2>
     <h3>What is Shopper List?</h3>
     <p>Shopper List is an app to track groceries you want to buy at various stores across your devices.</p>
-    <h3>How are lists stored?</h3>
+    <h3>Where are lists stored?</h3>
     <p>
       Currently lists are stored on device in your browser's Local Storage, however there are plans to allow for
       cross-device data storage via accounts. This is not implemented yet.
@@ -41,14 +62,18 @@
       {#each tutorials as tutorial}
         <Accordion.Item class="accordion-item">
           <Accordion.Header>
-            <Accordion.Trigger class="accordion-trigger invisible-button">
+            <Accordion.Trigger class="accordion-trigger light-button">
               <h3 class="accordion-header">
                 {tutorial.title} <span class="accordion-expand-icon"><IconCaretDownBold /></span>
               </h3>
             </Accordion.Trigger>
           </Accordion.Header>
           <Accordion.Content class="accordion-content">
-            <p>{tutorial.description}</p>
+            <div class="description">
+              {#each tutorial.description as section}
+                <p>{@html tutorial.description}</p>
+              {/each}
+            </div>
           </Accordion.Content>
         </Accordion.Item>
       {/each}
@@ -92,9 +117,15 @@
     }
   }
 
+  .description {
+    padding: var(--size-3) var(--size-5);
+  }
+
   p {
-    margin-bottom: var(--size-3);
     color: var(--text-2);
+    &:not(:last-child) {
+      margin-bottom: var(--size-3);
+    }
   }
 
   @keyframes accordion-open {
